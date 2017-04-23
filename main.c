34c2e2a38e940d237e42b4e6a1cf17537584e0b7
Sylvain Pouclet <sylvainpouclet@MacBook-Pro-de-Sylvain.local>
2017-04-23 17:57:05 +0200

    Tarea 1 Completa

--- main.c
+++ main.c
@@ -8,10 +8,10 @@
 
 void world_init(bool *w_init);
 void world_print(bool *w_print);
-void world_step(/* Recibo dos mundos */);
-int world_count_neighbors(/* Recibo un mundo y unas coordenadas */);
-bool world_get_cell(/* Recibo un mundo y unas coordenadas */);
-void world_copy(/* Recibo dos mundos */);
+void world_step(bool *w1_step , bool *w2_step);
+int world_count_neighbors(bool *w_count , int X_count , int Y_count);
+bool world_get_cell(bool *w_get , int X_get , int Y_get);
+void world_copy(bool *w1_copy , bool *w2_copy);
 
 int main()
 {
@@ -28,10 +28,13 @@ int main()
 	
 	do {
 		printf("\033cIteration %d\n", i++);
+		
 		// TODO: Imprime el mundo: OK
 		world_print(puntero_world1);
-		
-		// TODO: Itera
+
+		// TODO: Itera: OK
+		world_step(puntero_world1 , puntero_world2);
+
 	} while (getchar() != 'q');
 
 	return EXIT_SUCCESS;
@@ -40,7 +43,7 @@ int main()
 void world_init(bool *w_init)
 {
 	// TODO: Poner el mundo a false: OK
-	for (int i = 0 ; i < (W_SIZE_Y * W_SIZE_X ) ; i++)
+	for (int i = 0 ; i < (W_SIZE_Y * W_SIZE_X) ; i++)
 		{
 			*(w_init + i) = false;
 		}
@@ -83,10 +86,10 @@ void world_print(bool *w_print)
 	}
 }
 
-void world_step(/* Recibo dos mundos */)
+void world_step(bool *w1_step , bool *w2_step)
 {
 	/*
-	 * TODO:
+	 * TODO: OK
 	 * - Recorrer el mundo célula por célula comprobando si nace, sobrevive
 	 *   o muere.
 	 *
@@ -95,22 +98,84 @@ void world_step(/* Recibo dos mundos */)
 	 *
 	 * - Copiar el mundo auxiliar sobre el mundo principal
 	 */
+	for (int i = 0 ; i < W_SIZE_Y ; i++)
+	{
+		for (int j = 0; j < W_SIZE_X; ++j)
+		{
+		*(w2_step + (i * W_SIZE_X + j)) = world_get_cell(w1_step , j , i);
+		}
+	}
+
+	//Copiar el mundo auxiliar sobre el mundo principal: OK
+
+	world_copy(w1_step , w2_step);
+
 }
 
-int world_count_neighbors(/* Recibo un mundo y unas coordenadas */)
+int world_count_neighbors(bool *w_count , int X_count , int Y_count)
 {
-	// Devuelve el número de vecinos
+	int counter = 0;
+
+	if ( *(w_count + (Y_count * W_SIZE_X) + X_count - 1) && (X_count > 0))
+	{
+		counter++;
+	}
+	if ( *(w_count + (Y_count * W_SIZE_X) + X_count + 1) && (X_count < W_SIZE_X - 1))
+	{
+		counter++;
+	}
+	if ( *(w_count + ((Y_count - 1)  * W_SIZE_X) + X_count - 1) && (X_count > 0) && (Y_count > 0))
+	{
+		counter++;
+	}
+	if ( *(w_count + ((Y_count - 1)  * W_SIZE_X) + X_count) && (Y_count > 0))
+	{
+		counter++;
+	}
+	if ( *(w_count + ((Y_count - 1)  * W_SIZE_X) + X_count +1) && (Y_count > 0))
+	{
+		counter++;
+	}
+	if ( *(w_count + ((Y_count + 1)  * W_SIZE_X) + X_count - 1)  && (X_count > 0) && (Y_count < W_SIZE_Y - 1))
+	{
+		counter++;
+	}
+	if ( *(w_count + ((Y_count + 1)  * W_SIZE_X) + X_count) && (Y_count < W_SIZE_Y - 1))
+	{
+		counter++;
+	}
+	if ( *(w_count + ((Y_count + 1)  * W_SIZE_X) + X_count +1) && (X_count < W_SIZE_X - 1 ) && (Y_count < W_SIZE_Y - 1 ))
+	{
+		counter++;
+	}
+
+	// Devuelve el número de vecinos: OK
+	return counter;
 }
 
-bool world_get_cell(/* Recibo un mundo y unas coordenadas */)
+bool world_get_cell(bool *w_get , int X_get , int Y_get)
 {
 	/*
-	 * TODO: Devuelve el estado de la célula de posición indicada
+	 * TODO: Devuelve el estado de la célula de posición indicada: OK
 	 * (¡cuidado con los límites del array!)
 	 */
+	int neighbors = world_count_neighbors(w_get , X_get , Y_get);
+	
+	if ( *(w_get + (Y_get * W_SIZE_X) + X_get) && ( neighbors == 2 || neighbors == 3) || !*(w_get + (Y_get * W_SIZE_X) + X_get) && neighbors == 3 )
+	 {
+	 	return true;
+	 } 
+
+	 return false;
 }
 
-void world_copy(/* Recibo dos mundos */)
+void world_copy(bool *w1_copy , bool *w2_copy)
 {
-	// TODO: copia el mundo segundo mundo sobre el primero
+	// TODO: copia el mundo segundo mundo sobre el primero: OK
+	for (int i = 0 ; i < (W_SIZE_Y * W_SIZE_X) ; i++)
+		{
+			*(w1_copy + i) = *(w2_copy + i);
+		}
+		
+	
 }
