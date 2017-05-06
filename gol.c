966b9d2c28449072a3f39aeb103597a758fa4433
Sylvain Pouclet <sylvainpouclet@MacBook-Pro-de-Sylvain.local>
2017-05-06 13:12:21 +0200

    Simplificacion del acceso a los punteros

--- gol.c
+++ gol.c
@@ -6,23 +6,23 @@
 void world_init(struct world *w)
 {
 	// Init a false del mundo
-	for (int i = 0 ; i < W_SIZE_X ; i++)
-		for (int j = 0; j < W_SIZE_Y; ++j)
-			*(w->w_1[0] + (i * W_SIZE_Y + j)) = false;
+	for (int x = 0 ; x < W_SIZE_X ; x++)
+		for (int  y = 0; y < W_SIZE_Y; ++y)
+			w->w_1[x][y] = false;
 	
 	// Init de un Glider	
-	*(w->w_1[0] + (0 * W_SIZE_Y + 1)) = true;
-	*(w->w_1[0] + (1 * W_SIZE_Y + 2)) = true;	
-	*(w->w_1[0] + (2 * W_SIZE_Y + 0)) = true;
-	*(w->w_1[0] + (2 * W_SIZE_Y + 1)) = true;	
-	*(w->w_1[0] + (2 * W_SIZE_Y + 2)) = true;	
+	w->w_1[0][1] = true;
+	w->w_1[1][2] = true;	
+	w->w_1[2][0] = true;
+	w->w_1[2][1] = true;	
+	w->w_1[2][2] = true;	
 }
 
 void world_print(const struct world *w)
 {
-	for (int i = 0 ; i < W_SIZE_X ; i++) {
-		for (int j = 0; j < W_SIZE_Y; ++j)
-			printf("%c " , *(w->w_1[0] + (i * W_SIZE_Y + j)) == 0 ? '.' : '#' );	
+	for (int x = 0 ; x < W_SIZE_X ; x++) {
+		for (int y = 0; y < W_SIZE_Y; ++y)
+			printf("%c " , w->w_1[x][y] == 0 ? '.' : '#' );	
 
 		printf("\n");	
 	}
@@ -33,7 +33,7 @@ void world_step(struct world *w)
 	for (int x = 0 ; x < W_SIZE_X ; x++) {
 		for (int y = 0; y < W_SIZE_Y; ++y) {
 			int neighbors = world_count_neighbors(w, x, y);	
-			*(w->w_2[0] + (x * W_SIZE_Y + y)) = (*(w->w_1[0] + (x * W_SIZE_Y + y)) && neighbors == 2) || neighbors == 3;
+			w->w_2[x][y] = (w->w_1[x][y] && neighbors == 2) || neighbors == 3;
 		}
 	}
 	// copia del mundo auxiliar al mundo principal
@@ -69,12 +69,12 @@ bool world_get_cell(const struct world *w, int x , int y)
 	else if (y == W_SIZE_Y)
 		y = 0;
 
-	return *(w->w_1[0] + (x * W_SIZE_Y + y));
+	return w->w_1[x][y];
 }
 
 void world_copy(struct world *w)
 {
-	for (int i = 0 ; i < W_SIZE_X ; i++)
-		for (int j = 0; j < W_SIZE_Y; ++j)
-			*(w->w_1[0] + (i * W_SIZE_Y + j)) = *(w->w_2[0] + (i * W_SIZE_Y + j));
+	for (int x = 0 ; x < W_SIZE_X ; x++)
+		for (int y = 0; y < W_SIZE_Y; ++y)
+			w->w_1[x][y] = w->w_2[x][y];
 }
